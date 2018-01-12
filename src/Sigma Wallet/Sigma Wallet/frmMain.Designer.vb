1f0354e7ff9fb0a817d43e573e70621a1d39c5e8
frelis <luis.freire@gmail.com>
2018-01-12 16:14:58 +0100

    first app commit

---
+++ src/Sigma Wallet/Sigma Wallet/frmMain.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.flowpanel = New System.Windows.Forms.FlowLayoutPanel()
+        Me.SuspendLayout()
+        '
+        'flowpanel
+        '
+        Me.flowpanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.flowpanel.Location = New System.Drawing.Point(0, 0)
+        Me.flowpanel.Name = "flowpanel"
+        Me.flowpanel.Size = New System.Drawing.Size(284, 389)
+        Me.flowpanel.TabIndex = 0
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 389)
+        Me.Controls.Add(Me.flowpanel)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MinimumSize = New System.Drawing.Size(300, 427)
+        Me.Name = "frmMain"
+        Me.Text = "Σ Wallet"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents flowpanel As FlowLayoutPanel
+End Class
