66fa1ee0e367ff1714779e45b9de20c06423c7fa
frelis <luis.freire@gmail.com>
2018-01-18 16:45:38 +0100

    Shortening the link text for download, added error messages

--- src/Common/frmDownloadFile.vb
+++ src/Common/frmDownloadFile.vb
@@ -5,17 +5,16 @@ Public Class frmDownloadFile
     Public Function DownloadFile(from As String, dest As String) As Boolean
         Try
             Dim client As Net.WebClient = New Net.WebClient()
-            lblfile.Text = from
+            lblfile.Text = Shorten_text(from, lblfile)
             AddHandler client.DownloadProgressChanged, AddressOf client_DownloadProgressChanged
             AddHandler client.DownloadFileCompleted, AddressOf client_DownloadFileCompleted
             client.UseDefaultCredentials = True
             client.DownloadFileAsync(New Uri(from), dest)
             Me.ShowDialog()
-
             Return True
         Catch ex As Exception
             If Me.Visible Then Me.Close()
-            MsgBox("Falied to download:" + vbNewLine + from)
+            Log.Error("Download File", ex)
         End Try
         Return False
     End Function
@@ -23,7 +22,7 @@ Public Class frmDownloadFile
     Private Sub client_DownloadFileCompleted(sender As Object, e As AsyncCompletedEventArgs)
         Me.Close()
         If Not e.Error Is Nothing Then
-            MsgBox(e.Error.Message)
+            Log.Error("Download File Completed", e.Error)
         End If
     End Sub
 
@@ -32,4 +31,11 @@ Public Class frmDownloadFile
         Dim totalBytes As Long = e.TotalBytesToReceive
         ProgressBar1.Value = CInt(bytesIn / totalBytes * 100)
     End Sub
+
+    Private Function Shorten_text(txt As String, lbl As Windows.Forms.Label) As String
+        Dim textSize As Drawing.Size = Windows.Forms.TextRenderer.MeasureText(txt, lbl.Font)
+        If textSize.Width < lbl.Width Then Return txt
+        Dim txtp() As String = txt.Split("/"c)
+        Return txtp(0) + "//" + txtp(2) + "/../" + txtp(txtp.Length - 1)
+    End Function
 End Class
