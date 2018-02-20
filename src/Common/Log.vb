9e884f05756a7b7abda9fae6aa5e74417e761e70
frelis <luis.freire@gmail.com>
2018-02-20 23:39:10 +0100

    Make message box modeless

--- src/Common/Log.vb
+++ src/Common/Log.vb
@@ -1,10 +1,12 @@
 ﻿Public Class Log
     Shared Sub [Error](Origin As String, ex As Exception)
         Report.Error(Origin, ex)
-        MsgBox(Lang.Str("E_" + ex.Message + "_"), MsgBoxStyle.Critical, Lang.Str("T_" + Origin + "_"))
+        Dim t As Threading.Thread = New Threading.Thread(Sub() MsgBox(Lang.Str("E_" + ex.Message + "_"), MsgBoxStyle.Critical, Lang.Str("T_" + Origin + "_")))
+        t.Start()
     End Sub
 
     Public Shared Sub Warning(title As String, text As String)
-        MsgBox(Lang.Str("W_" + text + "_"), MsgBoxStyle.Information, Lang.Str("T_" + title + "_"))
+        Dim t As Threading.Thread = New Threading.Thread(Sub() MsgBox(Lang.Str("W_" + text + "_"), MsgBoxStyle.Information, Lang.Str("T_" + title + "_")))
+        t.Start()
     End Sub
 End Class
